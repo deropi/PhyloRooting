6f1953b0e500e3e994a9d56f3eb149f6630d76bf
Devani Romero Picazo <deropi@users.noreply.github.com>
2023-02-20 09:32:26 +0100

    Update Plots.R

--- Plots.R
+++ Plots.R
@@ -5,7 +5,7 @@ library("ggpubr")
 
 
 ## Fig 2c
-df <- read.table('Opist.df', header=TRUE, sep = ',')
+df <- read.table('Opisthokonta.df', header=TRUE, sep = ',')
 dfroot<- df %>% group_by(Tree) %>% 
   filter(AD==min(AD), GeneFam=="CSC")
 dfroot.2 <- as.data.frame(dcast(df, Tree ~ Partition_id, value.var="AD"))
@@ -18,6 +18,6 @@ ggplot(dfroot.2, aes(x=`1`, y=`2`)) + geom_hex(binwidth = c(0.035, 0.035)) + coo
 
 
 #Fig 4
-df <- read.table('Nextcloud/PhyloRoot/Proteobacteria.df', header=TRUE, sep = ',')
+df <- read.table('Proteobacteria.df', header=TRUE, sep = ',')
 ggplot(df , aes(x=as.numeric(AD), color=as.character(Partition_id)))+ stat_ecdf()+theme_bw() + xlab('AD') + ylab('Cumulative distribution') + facet_wrap(vars(GeneFam))
 
